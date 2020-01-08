aec7150813e09cde18e59061e64545066a1eafd5
Naouali <elnaoualinebil@gmail.com>
2020-01-08 19:26:57 +0100

    safe printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list = [], x=0):
+    z = 0
+    for i in range(x):
+        try:
+            print(my_list[z], end="")
+            z = z + 1
+        except IndexError:
+            pass
+    print("")
+    return z
