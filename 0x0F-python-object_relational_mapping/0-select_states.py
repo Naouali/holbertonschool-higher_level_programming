45db29953305dd3e5a834581f457f219e1e5ab9b
naouali <elnaoualinebil@gmail.com>
2020-03-30 16:37:29 +0100

    query states by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #selecting states from databases using mysql alchemy
-from sqlalchemy import engine
+from sqlalchemy import ceate_engine
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
 Base.metadata.create_all(engine)
