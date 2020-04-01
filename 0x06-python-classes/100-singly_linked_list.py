7936c978928e35928438cc29495b65eed96726d9
naouali <elnaoualinebil@gmail.com>
2020-04-02 00:04:27 +0100

    still not workin

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python3
 class Node:
-    def __init__(self, data, next_node=None):
-        if type(data) != int:
-            raise TypeError("data must be integer")
-        else:
-            self.__data = data
-        if type(next_node) != Node and type(next_node) != None:
-            raise TypeError("next_node must be a Node object")
-        else:
-            self.__next_node = next_node
+    """Create a Node obkect."""
+    def __init__(self, data, next_node=None): #self.next_node ??
+        """put Node data structrure"""
+        self.data = data
+        self.next_node = next_node
 
     @property
     def next_node(self):
+        """retrive next node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if type(value) != Node and type(value) != None:
+        """define next node parameters"""
+        if type(value) is not Node and type(value) is not None:
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
 
     @property
     def data(self):
+        """define data"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """define data structure"""
         if type(value) != int:
             raise TypeError("data must be integer")
         else:
             self.__data = value
 
+    def __str__(self):
+        """ convert node to string"""
+        return str(self.__data)
+    
 class SinglyLinkedList:
+    """define a linked list class """
     def __init__(self):
-        self.head = None
+        """initialize a singly linked list """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new = Node(value)
+        curr = self.__head
+        
 #rest is to add nodes/ then retrive all the data inserted and print it sorted one per line        
