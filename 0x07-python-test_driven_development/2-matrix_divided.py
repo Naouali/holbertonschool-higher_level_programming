c87cf461ebc97e936fba5284d7b168e674ae0faa
naouali <elnaoualinebil@gmail.com>
2020-04-05 19:56:38 +0100

    errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,14 +5,17 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     if type(div) != int and type(div) != float:
         raise TypeError("div must be an integer")
+    msg2 = " Each row of the matrix must have the same size"
     msg = "matrix must be a matrix (list of lists) of integers/floats"
+    for i in range(len(matrix)-1):
+        if len(matrix[i]) != len(matrix[i + 1]):
+            raise TypeError(msg2)
     new = []
     for i in matrix:
         mini = []
         for j in i:
             if type (j) != int and type(j) != float:
                 raise TypeError(msg)
-                break
             z = round(j/div, 2)
             mini.append(z)
         new.append(mini)
