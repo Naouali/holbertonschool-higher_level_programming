42580b44267db82e502b4aa690c6d8fd56a0dd61
naouali <elnaoualinebil@gmail.com>
2020-04-05 21:02:33 +0100

    doc strings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,14 +7,17 @@ rectangle module
 class Rectangle:
     """class to create a rectangle"""
     def __init__(self, width=0, height=0):
+        """define a rectange by its width and height"""
         self.width = width
         self.height = height
 
     @property
+    """retrive the width attribute"""
     def width(self):
         return self.__widht
 
     @width.setter
+    """set the width sttribute"""
     def width(self, value):
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -24,10 +27,12 @@ class Rectangle:
             self.__width = value
 
     @property
+    """retrive the height"""
     def height(self):
         return self.__height
 
     @height.setter
+    """set the height attribue"""
     def height(self, value):
         if value < 0:
             raise ValueError("width must be >= 0")
