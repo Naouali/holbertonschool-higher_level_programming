75a8b6912cba37c326d702594691599ecf90b7ea
naouali <elnaoualinebil@gmail.com>
2020-04-05 20:16:30 +0100

    format

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module with function print my name
+See docstrings
+"""
 def say_my_name(first_name, last_name=""):
     """ print my name is <first> + <last>"""
     if type(first_name) != str:
@@ -6,4 +10,4 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) != str:
         raise TypeError("last_name must be a string")
     else:
-        print("my name is" + " " + first_name + " " + last_name)
+        print("my name is {} {}".format(first_name, last_name))
