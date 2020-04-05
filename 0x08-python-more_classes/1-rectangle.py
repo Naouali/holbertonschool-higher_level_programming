3a154d62d9eacf57bf31482a8bf55eb27be292ba
naouali <elnaoualinebil@gmail.com>
2020-04-05 20:57:36 +0100

    rectangle 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+rectangle module
+"""
+
+
+class Rectangle:
+    """class to create a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__widht
+
+    @width.setter
+    def width(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        elif type(value) != int:
+            raise TypeError("width must be an integer")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        elif type(value) != int:
+            raise TypeError("width must be an integer")
+        else:
+            self.__height = value
