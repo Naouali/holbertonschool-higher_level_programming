021e593637bbdedce3efe42a3e809ffc66871554
naouali <elnaoualinebil@gmail.com>
2020-04-06 22:29:25 +0100

    revers logic

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -4,7 +4,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as f:
-        data = f.write(str(my_obj))
-        new = json.dumps(data)
-        return new
-        
+        data = json.dumps(my_obj)
+        f.write(data)
