0d304869cc27f8ff00c58081c2e7068f83f370b5
Naouali <elnaoualinebil@gmail.com>
2020-04-24 20:44:25 +0100

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ Class Base:
+        private class attribute __nb_objects = 0
+        class constructor: def __init__(self, id=None)::
+"""
+
+
+class Base:
+    """
+    base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
