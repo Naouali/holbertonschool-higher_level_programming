0465854060c12a2850dfb02163ce39f4e1839adc
Naouali <elnaoualinebil@gmail.com>
2020-04-27 22:15:38 +0100

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script to create a use
 -- and give him some privileges
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY "user_0d_pwd";
+CREATE USER IF NOT EXISTS "user_0d_1@localhost" IDENTIFIED BY "user_0d_pwd";
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1@localhost";
