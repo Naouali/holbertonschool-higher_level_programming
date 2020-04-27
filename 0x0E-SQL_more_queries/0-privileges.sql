9823256d9bb46cefde1ec99ed0c283e0647af86c
Naouali <elnaoualinebil@gmail.com>
2020-04-27 22:22:37 +0100

    user@localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Show privileges of a user
 -- another comment
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR "user_0d_1"@"localhost";
+SHOW GRANTS FOR "user_0d_2"@"localhost";
