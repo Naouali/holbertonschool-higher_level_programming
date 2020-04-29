6810e05ec15c2fb72958f256e0f349fcf590c5fd
Naouali <elnaoualinebil@gmail.com>
2020-04-29 23:13:17 +0100

    del

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,42 +1,62 @@
 #!/usr/bin/python3
 """
-module to print a rectangle
+area and rectange module
 """
 
 
-class rectangle:
-	"""class to print
-	rectagle
-	"""
-	def __init__(self, width=0, height=0):
-        self.widht = width
+class Rectangle:
+    """ Rectangle class
+    width : rectangle width
+    height: rectange height
+    """
+    def __init__(self, width=0, height=0):
         self.height = height
+        self.width = width
+
+    def __del__(self):
+        """
+        method called
+        every time insatnce
+        deleted
+        """
+        print("Bye rectangle...")
 
     def area(self):
-        if self.__width == 0 or self.__height == 0:
-            return (0)
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return self.__width * 2 + self.__height * 2
 
     def __str__(self):
         s = ""
-        if self.__width == 0 or self.__height == 0:
+        if self.__height == 0 or self.__width == 0:
             return s
         for i in range(self.__height):
             s += ("#" * self.__width + "\n")
         return s[:-1]
 
-    def __repr(self):
-        
+    def __repr__(self):
+        """return a string represenation
+        of the rectange instance
+        """
+        return 'Rectangle({}, {})'.format(self.__width, self.height)
 
-    @proprety
+    @property
     def width(self):
+        """retrive the rectange widht:
+        Return:
+        int
+        """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
+        """set rectange width
+        Args:
+        int
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -45,15 +65,20 @@ class rectangle:
 
     @property
     def height(self):
+        """retrive rectanle height
+        Return:
+        int
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """set rectangle height
+        Args:
+        int
+        """
         if type(value) != int:
-            raise TYpeError("height must be an integer")
+            raise TypeError("height must be and integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height musy be >= 0")
         self.__height = value
-
-        
-		
