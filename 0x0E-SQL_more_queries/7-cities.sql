a545cc7d8e55d539ddce2fa181773ae85aa9d2de
Naouali <elnaoualinebil@gmail.com>
2020-04-30 23:10:25 +0100

    cities created

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,5 @@
--- create database
--- create table inside it
+-- create cities
+-- cities states_id reference to states id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT,
-states_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(ID), FOREIGN KEY(states_id) REFERENCES
-hbtn_0d_usa.states(id));
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256), FOREIGN KEY(state_id) REFERENCES states(id));
