b5cc96b676f6dbebe9030ff746e0780c1bb726c2
Naouali <elnaoualinebil@gmail.com>
2020-04-30 23:40:45 +0100

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- create cities
 -- cities states_id reference to states id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256), FOREIGN KEY(state_id) REFERENCES states(id));
+-- USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256), FOREIGN KEY(state_id) REFERENCES states(id));
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- list california
+-- cities
+SELECT id, name FROM cities WHERE state_id = (
+  SELECT id FROM states
+  WHERE name = "California")
+ORDER BY cities.id ASC;
