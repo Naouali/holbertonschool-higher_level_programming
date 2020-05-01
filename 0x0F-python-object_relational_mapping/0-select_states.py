f82b697e213bddacb4ac4810a11d464a39ed15b2
Naouali <elnaoualinebil@gmail.com>
2020-05-01 01:59:30 +0100

    if name = main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,20 +3,20 @@
 list all states
 """
 
-
-from MySQLdb import _mysql
-import MySQLdb
-import sys
-db = MySQLdb.connect(
+if __name__ == "__main__":
+    from MySQLdb import _mysql
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(
         host="localhost",
-        port=3306, 
+        port=3306,
         db=sys.argv[3],
         passwd=sys.argv[2],
-        user = sys.argv[1])
-c = db.cursor()
-c.execute("""SELECT * FROM states ORDER BY id ASC""")
-st = c.fetchall()
-for s in st:
-    print(s)
-c.close()
-db.close()
+        user=sys.argv[1])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    st = c.fetchall()
+    for s in st:
+        print(s)
+    c.close()
+    db.close()
