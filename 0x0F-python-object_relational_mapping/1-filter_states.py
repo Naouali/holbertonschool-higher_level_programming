599187cf271580b797e4a6ab6f0deb7624f9e8be
Naouali <elnaoualinebil@gmail.com>
2020-05-01 02:02:43 +0100

    start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+list all states
+"""
+
+if __name__ == "__main__":
+    from MySQLdb import _mysql
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        db=sys.argv[3],
+        passwd=sys.argv[2],
+        user=sys.argv[1])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    st = c.fetchall()
+    for s in st:
+        if s[1][0] == "N":
+            print(s)
+    c.close()
+    db.close()
