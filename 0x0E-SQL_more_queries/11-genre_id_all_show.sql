bfe3c5629428a38e1afdfbfeceec52b37d7985da
Naouali <elnaoualinebil@gmail.com>
2020-05-03 21:37:27 +0100

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_show.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_show.sql
@@ -1,3 +1,6 @@
 -- more join
 -- list shows
-SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM title INNER JOIN genre_id ON 
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+  FROM tv_shows LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
