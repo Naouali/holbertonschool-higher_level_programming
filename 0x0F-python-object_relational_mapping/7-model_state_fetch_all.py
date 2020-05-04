82cafacbd7f68bf0787fdbf0d67404f01e377b18
Naouali <elnaoualinebil@gmail.com>
2020-05-04 01:14:56 +0100

    fetch data

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module to fetch
+all states
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+import sys
+engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
+    sys.argv[1], sys.argv[2], sys.argv[3]))
+res = engine.execute("SELECT id, name FROM states")
+
+for x in res:
+    print("{}: {}".format(x[0], x[1]))
