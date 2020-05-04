78597d835fab4f3cc3c0d3be3531daf0797f18cf
Naouali <elnaoualinebil@gmail.com>
2020-05-04 01:23:08 +0100

    if name = main

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,14 +5,13 @@ all states
 """
 
 
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-import sys
-engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
-    sys.argv[1], sys.argv[2], sys.argv[3]))
-res = engine.execute("SELECT id, name FROM states ORDER BY\
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    import sys
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    res = engine.execute("SELECT id, name FROM states ORDER BY\
         states.id ASC")
-if len(res) = 0:
-    print()
-for x in res:
-    print("{}: {}".format(x[0], x[1]))
+    for x in res:
+        print("{}: {}".format(x[0], x[1]))
