3b1c11270d006562c2cf79fb114910a74d1dbd9b
Naouali <elnaoualinebil@gmail.com>
2020-05-04 01:21:07 +0100

    empty

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,6 +12,7 @@ engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
     sys.argv[1], sys.argv[2], sys.argv[3]))
 res = engine.execute("SELECT id, name FROM states ORDER BY\
         states.id ASC")
-
+if len(res) = 0:
+    print()
 for x in res:
     print("{}: {}".format(x[0], x[1]))
