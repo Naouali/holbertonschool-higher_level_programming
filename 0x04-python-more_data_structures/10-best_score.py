cd7b56209e61260fd6586bba1efd63f48df20a11
Naouali <elnaoualinebil@gmail.com>
2020-05-04 03:02:30 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    v = list(a_dictionary.values())
-    k = list(a_dictionary.keys())
-    return (k[v.index(max(v))])
+    if a_dictionary is None:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
