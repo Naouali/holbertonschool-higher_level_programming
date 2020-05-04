694d372836647457e870b5a444fd3663e567d7ac
Naouali <elnaoualinebil@gmail.com>
2020-05-04 01:16:25 +0100

    order by

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,8 @@ from sqlalchemy import create_engine
 import sys
 engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
     sys.argv[1], sys.argv[2], sys.argv[3]))
-res = engine.execute("SELECT id, name FROM states")
+res = engine.execute("SELECT id, name FROM states ORDER BY\
+        states.id ASC")
 
 for x in res:
     print("{}: {}".format(x[0], x[1]))
