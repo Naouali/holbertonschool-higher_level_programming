f15e1ddc630236b9f372cbee5bc9e059bee677dd
Naouali <elnaoualinebil@gmail.com>
2020-05-05 16:05:52 +0100

    m

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const len = args.length;
-if (len > 0) {
+if (len === 1) {
   console.log('Argument found');
-} else {
+} else if (len === 0) {
   console.log('No argument');
+}else {
+  console.log('Arguments found');
 }
