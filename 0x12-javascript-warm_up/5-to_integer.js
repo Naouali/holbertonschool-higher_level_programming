91e62302a46f668eadd23f136d5d0a919ab784e8
Naouali <elnaoualinebil@gmail.com>
2020-05-05 23:24:30 +0100

    space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const first = parseInt(process.argv[2]);
 if (isNaN(first)) {
   console.log('Not a number');
 } else {
-  console.log('My number:' + first);
+  console.log('My number: ' + first);
 }
