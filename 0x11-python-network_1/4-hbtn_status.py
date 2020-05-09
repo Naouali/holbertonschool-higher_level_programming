ba4466ff2b7fa2bc69a2f8042bef9a329d955bd5
Naouali <elnaoualinebil@gmail.com>
2020-05-09 09:53:13 +0100

    if name

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,8 @@ url
 import requests
 
 
-data = requests.get("https://intranet.hbtn.io/status/")
-print("Body response:")
-print("\t - type: {}".format(type(data.text)))
-print("\t - content: {}".format(data.text))
+if __name__ = "__main__":
+    data = requests.get("https://intranet.hbtn.io/status/")
+    print("Body response:")
+    print("\t - type: {}".format(type(data.text)))
+    print("\t - content: {}".format(data.text))
