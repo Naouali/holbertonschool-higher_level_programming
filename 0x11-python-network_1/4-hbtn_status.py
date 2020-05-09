0882cc193ee794bffaf86900434b3be40f82de31
Naouali <elnaoualinebil@gmail.com>
2020-05-09 09:56:02 +0100

    space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,5 +11,5 @@ import requests
 if __name__ == "__main__":
     data = requests.get("https://intranet.hbtn.io/status/")
     print("Body response:")
-    print("\t - type: {}".format(type(data.text)))
-    print("\t - content: {}".format(data.text))
+    print("\t- type: {}".format(type(data.text)))
+    print("\t- content: {}".format(data.text))
