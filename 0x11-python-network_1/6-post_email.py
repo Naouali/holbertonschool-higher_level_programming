121ba1497e75fabec297c1e3e2a8760eed3a0554
Naouali <elnaoualinebil@gmail.com>
2020-05-09 13:02:05 +0100

    if

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,8 @@ data
 import requests
 import sys
 
-payload = {"email": str(sys.argv[2])}
-data = requests.post(str(sys.argv[1]+"/post"), data=payload)
-print("you email is: {}".format(data.text["email"]))
+
+if __name__ == "__main__":
+    payload = {"email": str(sys.argv[2])}
+    data = requests.post(str(sys.argv[1]+"/post"), data=payload)
+    print("you email is: {}".format(data.text["email"]))
