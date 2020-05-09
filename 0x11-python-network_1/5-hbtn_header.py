8da1e716d48226594f261285b422c913a6a5188f
Naouali <elnaoualinebil@gmail.com>
2020-05-09 10:06:54 +0100

    argv

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,9 @@ fetch headeds
 
 
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    data = requests.get("https://intranet.hbtn.io/status")
+    data = requests.get(str(sys.argv[1]))
     print(data.headers["X-Request-Id"])
