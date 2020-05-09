4468cc3a69a025381892fa338dd4b7bb9c945a42
Naouali <elnaoualinebil@gmail.com>
2020-05-09 10:32:57 +0100

    check status

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+error codes
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = requests.get(str(sys.argv[1]))
+    if data.status_code >= 400:
+        print("Error code: {}".format(data.status_code))
+    else:
+        print("Index")
