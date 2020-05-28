4107e88c677c091b057ebab3d8924109302c2bc6
Naouali <elnaoualinebil@gmail.com>
2020-05-28 20:34:13 +0100

    no filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -11,5 +11,11 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            dic = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dic[i] = self.__dict__[i]
+            return dic
         return (self.__dict__)
