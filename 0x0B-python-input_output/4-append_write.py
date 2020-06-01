7b7dc92c8cf4608dc9a223f31c37d59b9f9cdec8
Naouali <elnaoualinebil@gmail.com>
2020-06-01 13:26:17 +0100

    docs

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+4
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    function
+    to append line
+    """
     with open(filename, 'a') as f:
         new = f.write(text)
         return new
