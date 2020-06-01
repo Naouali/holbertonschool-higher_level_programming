57f19c66a181e67c368c7cb65a7d70721d1e2719
Naouali <elnaoualinebil@gmail.com>
2020-06-01 13:20:06 +0100

    documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,10 @@ module to read files
 
 
 def read_file(filename=""):
+    """
+    function to 
+    read file
+    """
     with open(filename) as f:
         for i in f:
             print(i, end="")
