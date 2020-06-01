e5f9535cca2560e7c8de132c8c5c61741188a59e
Naouali <elnaoualinebil@gmail.com>
2020-06-01 13:25:05 +0100

    docs

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+3
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    write into
+    file
+    """
     with open(filename, 'w') as f:
         new = f.write(text)
         return new
