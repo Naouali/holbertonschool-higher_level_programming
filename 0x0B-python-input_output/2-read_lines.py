ec16ce13d910a87cd05688b2f95272724a375ede
Naouali <elnaoualinebil@gmail.com>
2020-06-01 13:23:44 +0100

    docs

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+2
+"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """
+    read lines
+    """
     with open(filename) as f:
         if nb_lines <= 0:
             for line in f:
