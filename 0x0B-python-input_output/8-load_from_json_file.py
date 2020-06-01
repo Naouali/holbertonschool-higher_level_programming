1f27d3758f7571262362707f870aa902674993b3
Naouali <elnaoualinebil@gmail.com>
2020-06-01 13:31:08 +0100

    load

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""
+8
+"""
+
+
 import json
 
 
 def load_from_json_file(filename):
+    """
+    load from json
+    """
     with open(filename, 'r') as f:
         data = f.read()
         return (json.loads(data))
