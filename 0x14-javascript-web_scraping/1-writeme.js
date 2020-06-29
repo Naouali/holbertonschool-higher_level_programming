24937f39a7015a08b026a325e9b9c7d996a89f64
Naouali <elnaoualinebil@gmail.com>
2020-06-29 12:59:49 +0100

    error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], function (err) {
-  return console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], (error) => {
+  if (error) throw error;
 });
