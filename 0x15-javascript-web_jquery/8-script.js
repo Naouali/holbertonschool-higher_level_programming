3044159db9c97ac06d6e2563bd71dd8b987154d2
Naouali <elnaoualinebil@gmail.com>
2020-07-04 00:27:14 +0100

    fetching

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
+	$.each(data.results, function (key, val) {
+
+		$('#list_movies').append('<li>' + val.title + '</li>');
+	});
+});
