5c097dc4c0025face234a7d70fbd3f5938e6c35b
Naouali <elnaoualinebil@gmail.com>
2020-07-05 16:49:26 +0100

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-let f = process.argv[2];
-function factorial(n) {
-  if (n === 1) {
+const f = process.argv[2];
+function factorial (f) {
+  if (f > 1) {
+    return (f * factorial(f - 1));
+  } else if (f === 1) {
     return 1;
-  } else {
-    return (n * factorial(n - 1));
   }
+}
+if (f === undefined || !typeof (f)) {
+  console.log(1);
+} else {
   console.log(factorial(f));
+}
